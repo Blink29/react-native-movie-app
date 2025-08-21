10a032db6ba31144a636d20075bf57a080ee1cdd
Blink29 <purukumar2905@gmail.com>
2025-08-21 09:41:37 +0530

    appwrite connection working

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -4,8 +4,9 @@ const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
 const client = new Client()
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+  .setEndpoint("https://fra.cloud.appwrite.io/v1")
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
+  .setPlatform("com.blink.movieapp");
 
 const database = new Databases(client);
 
