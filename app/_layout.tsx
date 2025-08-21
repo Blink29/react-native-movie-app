67bd1990a7dd99ddbb3e093e1b5572108d7f7624
Blink29 <purukumar2905@gmail.com>
2025-08-21 09:19:43 +0530

    hide bottom navbar andriod

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,13 @@
+import * as NavigationBar from 'expo-navigation-bar';
+import { useEffect } from 'react';
 import { Stack } from "expo-router";
 import "./globals.css";
 import { StatusBar } from "react-native";
 
 export default function RootLayout() {
+  useEffect(() => {
+    NavigationBar.setVisibilityAsync("hidden");
+  }, []);
   return (
     <>
       <StatusBar hidden={true} />
